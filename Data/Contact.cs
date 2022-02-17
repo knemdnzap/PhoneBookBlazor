119b1bc0a42a4470ba30f3a07b78eada9101fcb8
Kevin Nicolas Mendoza Nuñez <knmendozan@siesa.com>
2022-02-17 17:42:45 -0500

    First Commit

---
+++ Data/Contact.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Data
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public int Phone { get; set; }
+        public int CellPhone { get; set; }
+    }
+}
